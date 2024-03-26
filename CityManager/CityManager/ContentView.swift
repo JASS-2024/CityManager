eb0321a4d293bf6dd5428230c1927b13871c6530
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2024-03-26 16:28:44 +0200

    Implement basic chat UI

--- CityManager/CityManager/ContentView.swift
+++ CityManager/CityManager/ContentView.swift
@@ -5,20 +5,63 @@
 //  Created by Julian Kraus on 26.03.24.
 //
 
+import Combine
 import SwiftUI
 
 struct ContentView: View {
+    @State var messages = DataSourceEmpty.messages
+    @State var newMessage: String = "This is a new message"
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            VStack {
+                ScrollViewReader { proxy in
+                ScrollView {
+                    LazyVStack {
+                        ForEach(messages, id: \.self) { message in
+                            MessageView(currentMessage: message)
+                                .id(message)
+                        }
+                    }
+                    .onReceive(Just(messages)) { _ in
+                        withAnimation {
+                            proxy.scrollTo(messages.last, anchor: .bottom)
+                        }
+                        
+                    }.onAppear {
+                        withAnimation {
+                            proxy.scrollTo(messages.last, anchor: .bottom)
+                        }
+                    }
+                }
+                
+                // send new message
+                HStack {
+                    Button(action: sendMessage)   {
+                        Image(systemName: "mic")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .padding()
+            }
         }
-        .padding()
+        
+        
+        
+    }
+    
+    func sendMessage() {
+        
+        if !newMessage.isEmpty{
+            messages.append(Message(content: newMessage, isCurrentUser: true))
+            messages.append(Message(content: "Reply of " + newMessage , isCurrentUser: false))
+            newMessage = "This is a new message"
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
