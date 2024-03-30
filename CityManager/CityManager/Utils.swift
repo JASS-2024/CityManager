76320ee5bfca13de259f6f04b46ec1ced31c85d6
Julian Kraus <>
2024-03-30 11:27:28 +0200

    change server link

--- CityManager/CityManager/Utils.swift
+++ CityManager/CityManager/Utils.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 class Utils {
-    static let link = "http://192.168.3.75:8080/request"//"http://192.168.3.250/test"
+    static let link = "http://192.168.3.250:8080/request"//"http://192.168.3.250/test"
     
-    static let plates = ["ABCD", "123", "AV2G"]
+    static let plates = ["ABC123",
+                         "ABC133",
+                         "BBA123",
+                         "BBA133",
+                         "CBC123"]
     
     static let stopWords = ["cancel", "terminate", "abort", "halt", "stop", "end", "cease", "discontinue", "suspend", "quit", "not", "don't", "never"]
 }
